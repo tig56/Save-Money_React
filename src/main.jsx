a6272afe7ea065e96cca9180b7e18d46622833b4
山田さん <yamadasan@yamadasannoMacBook-Air.local>
2025-05-24 07:42:24 +0900

    コードの変更

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Save-Money_React">
       <App />
     </BrowserRouter>
   </React.StrictMode>
